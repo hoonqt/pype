5ad2d3e36f3b0a6e7d07ffdb32898c0e6987294f
hoonqt <qitai_hoon@mymail.sutd.edu.sg>
2017-11-27 21:32:13 +0800

    Works for shape 0

--- colorpicker.py
+++ colorpicker.py
@@ -1,8 +1,9 @@
 from PIL import Image
 
 allwhites = {}
+allblacks = {}
 
-im = Image.open('shape2.png')
+im = Image.open('s0o0.bmp')
 rgb_im = im.convert('RGB')
 
 #file = open("shape2textr.txt","w")
@@ -12,22 +13,59 @@ rgb_im = im.convert('RGB')
 width,height = im.size
 
 current = (0,0,0)
-startpoint = 0
+whitestartpoint = 0
+blackstartpoint = 0
 
 for i in range(height):
+	whitestartpoint = 0
+	blackstartpoint = 0
 	for j in range(width):
 		r, g, b = rgb_im.getpixel((j, i))
 		if (r == 255 & g == 255 & b == 255):
 			if i not in allwhites:
 				allwhites[i] = [j]
 			else:
-				if len(allwhites[i]) == 1:
+				if len(allwhites[i])%2 == 1:
 					allwhites[i].append(j)
 				else:
-					if (allwhites[i][startpoint+1] +1 != j):
+					if (allwhites[i][whitestartpoint+1] +1 != j):
 						allwhites[i].append(j)
-						startpoint = 2
+						whitestartpoint = whitestartpoint + 2
 					else:
-						allwhites[i][startpoint + 1] = j
+						allwhites[i][whitestartpoint + 1] = j
+
+		elif (r == 0 & g == 0 & b == 0):
+			if i not in allblacks:
+				allblacks[i] = [j]
+			else:
+				if len(allblacks[i])%2 == 1:
+					allblacks[i].append(j)
+				else:
+					if (allblacks[i][blackstartpoint+1] + 1 == j):
+						allblacks[i][blackstartpoint+1] = j
+					else:
+						allblacks[i].append(j)
+						blackstartpoint +=2
+
+
+for keys in allwhites:
+	for i in range(len(allwhites[keys])):
+		allwhites[keys][i] = allwhites[keys][i] + 300
+
+for keys in allwhites:
+	if len(allwhites[keys]) == 1:
+		allwhites[keys].append(allwhites[keys][0]+1)
+	else:
+		difference = allwhites[keys][1] - allwhites[keys][0]
+		allwhites[keys][1] = allwhites[keys][1] + (allwhites[keys][1] - 325)
+		allwhites[keys][0] = allwhites[keys][0]- (325-allwhites[keys][0])
 
 print allwhites
+
+donald = ""
+
+for keys in allwhites:
+	print "wire line" + str(keys) +" = (((CounterX>=" + str(allwhites[keys][0]) + ")&(CounterX<=" + str(allwhites[keys][1]) + "))&(CounterY == " + str(keys) + "));"
+	donald = donald + "line" + str(keys) + " | "
+
+print donald
