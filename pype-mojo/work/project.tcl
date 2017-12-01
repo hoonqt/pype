8101d4a3f5964e7689b86fc118d2a7f56f9c5c93
hoonqt <qitai_hoon@mymail.sutd.edu.sg>
2017-12-02 01:51:33 +0800

    Add files via upload

--- pype-mojo/work/project.tcl
+++ pype-mojo/work/project.tcl
@@ -1,13 +1,13 @@
-set projDir "C:/Users/Joel/GitHub/pype/pype-mojo/work/planAhead"
+set projDir "C:/Users/1002357/Desktop/pype536/work/planAhead"
 set projName "pype"
 set topName top
 set device xc6slx9-2tqg144
 if {[file exists "$projDir/$projName"]} { file delete -force "$projDir/$projName" }
 create_project $projName "$projDir/$projName" -part $device
 set_property design_mode RTL [get_filesets sources_1]
-set verilogSources [list "C:/Users/Joel/GitHub/pype/pype-mojo/work/verilog/mojo_top_0.v" "C:/Users/Joel/GitHub/pype/pype-mojo/work/verilog/hvsync_generator_1.v"]
+set verilogSources [list "C:/Users/1002357/Desktop/pype536/work/verilog/mojo_top_0.v" "C:/Users/1002357/Desktop/pype536/work/verilog/hvsync_generator_1.v" "C:/Users/1002357/Desktop/pype536/work/verilog/tilesort_2.v" "C:/Users/1002357/Desktop/pype536/work/verilog/bannerdraw_3.v" "C:/Users/1002357/Desktop/pype536/work/verilog/tiledraw_4.v" "C:/Users/1002357/Desktop/pype536/work/verilog/tiledraw_4.v" "C:/Users/1002357/Desktop/pype536/work/verilog/tiledraw_4.v" "C:/Users/1002357/Desktop/pype536/work/verilog/tiledraw_4.v" "C:/Users/1002357/Desktop/pype536/work/verilog/tiledraw_4.v" "C:/Users/1002357/Desktop/pype536/work/verilog/bannerpart1_9.v" "C:/Users/1002357/Desktop/pype536/work/verilog/bannerpart2_10.v" "C:/Users/1002357/Desktop/pype536/work/verilog/bannerword1_11.v" "C:/Users/1002357/Desktop/pype536/work/verilog/bannerword2_12.v" "C:/Users/1002357/Desktop/pype536/work/verilog/bannerword3_13.v" "C:/Users/1002357/Desktop/pype536/work/verilog/bannerword4_14.v" "C:/Users/1002357/Desktop/pype536/work/verilog/bram_15.v" "C:/Users/1002357/Desktop/pype536/work/verilog/bram_15.v" "C:/Users/1002357/Desktop/pype536/work/verilog/bram_15.v" "C:/Users/1002357/Desktop/pype536/work/verilog/bram_15.v" "C:/Users/1002357/Desktop/pype536/work/verilog/bram_15.v"]
 import_files -fileset [get_filesets sources_1] -force -norecurse $verilogSources
-set ucfSources [list "C:/Users/Joel/GitHub/pype/pype-mojo/constraint/mojo.ucf"]
+set ucfSources [list "C:/Users/1002357/Desktop/pype536/constraint/mojo.ucf"]
 import_files -fileset [get_filesets constrs_1] -force -norecurse $ucfSources
 set_property -name {steps.bitgen.args.More Options} -value {-g Binary:Yes -g Compress} -objects [get_runs impl_1]
 set_property steps.map.args.mt on [get_runs impl_1]
